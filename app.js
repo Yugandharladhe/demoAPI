737df93ff4d8bb77aecb64ff42318c534d8c4e47
Yugandharladhe <yugandharladhe74@gmail.com>
2023-03-23 17:01:05 +0530

    third commit

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express=require("express")
 const app=express()
+const path=require("path")
+const QRCode=require("qrcode")
 var port=process.env.PORT||3000
 app.get("/",(req,res)=>{
-    res.send("<h1>hello my application has been deployed</h1>");
+    res.render("index")
 })
 
+app.get("/code",async(req,res)=>{
+    const obj=[{
+        menuItem:"xyz",
+        quantity:3,
+        instruction:"less"
+    },
+    {
+        menuItem:"pqr",
+        quantity:1,
+        instruction:"more"
+    },
+    {
+        menuItem:"pqr",
+        quantity:2,
+        instruction:"normal"
+    },
+    {
+        menuItem:"yzt",
+        quantity:3,
+        instruction:"extreme"
+    }];
+    const src=await QRCode.toDataURL(obj)
+    res.render("index",{src:src,stmt:"Scan this QR Code"})
+})
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"view"))
 app.listen(port,()=>{
     console.log("app is listening")
 })
