a78fd162283581234f2e5f1d6be88ac227dbb2e9
Yugandharladhe <yugandharladhe74@gmail.com>
2023-03-23 17:33:14 +0530

    successfull

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const app=express()
 const path=require("path")
 const QRCode=require("qrcode")
 var port=process.env.PORT||3000
+
+app.set("view engine","hbs")
+app.set("views",path.join(__dirname,"view"))
 app.get("/",(req,res)=>{
     res.render("index")
 })
 
 app.get("/code",async(req,res)=>{
     const src=await QRCode.toDataURL(JSON.stringify({name:"yugandhar",lname:"ladhe"}))
-    res.render("index")
+    res.render("index",{source:src})
 })
 
-app.set("view engine","ejs")
-app.set("views",path.join(__dirname,"view"))
+
 app.listen(port,()=>{
     console.log("app is listening")
 })
